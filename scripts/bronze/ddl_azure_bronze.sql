39ec9781bc1a020149568a09972d31c7a1a7c614
Paulina Fer <108699589+Paul1naFer@users.noreply.github.com>
2026-09-17 13:29:14 +0200

    Update ddl_azure_bronze.sql

--- scripts/bronze/ddl_azure_bronze.sql
+++ scripts/bronze/ddl_azure_bronze.sql
@@ -1,4 +1,15 @@
----- script for azure
+---- SCRIPT FOR AZURE SQL --------
+
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 CREATE TABLE bronze.crm_cust_info (
     cst_id              INT,
     cst_key             NVARCHAR(50),
